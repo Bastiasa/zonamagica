0952df05f2e02146d1b69057c24204414d0b5968
Bastiasa <leysantqm@gmail.com>
2026-08-17 00:00:04 -0500

    Remove basePath

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
     /* config options here */
 
-    basePath: "/zonamagica",
+    basePath: "/",
 
     images: {
         remotePatterns: [
