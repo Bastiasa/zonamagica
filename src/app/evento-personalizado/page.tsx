b52a3d43ff5658444711a6696fa5e1590aa89d6f
Bastiasa <leysantqm@gmail.com>
2026-08-19 17:57:36 -0500

    Fix layout

--- src/app/evento-personalizado/page.tsx
+++ src/app/evento-personalizado/page.tsx
@@ -1,10 +1,9 @@
 import { BuildYourPlanSection } from "@/src/components/buildYourPlan/BuildYourPlanSection";
-import { CenteredSection } from "@/src/components/CenteredSection";
 
 export default () => {
     return (
-        <CenteredSection>
+        <>
             <BuildYourPlanSection />
-        </CenteredSection>
+        </>
     );
 };
