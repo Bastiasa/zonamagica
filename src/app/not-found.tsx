1111ff6527c54fe42cf5275fe841aa63708ed3a9
Bastiasa <leysantqm@gmail.com>
2026-08-20 04:02:31 -0500

    Not found rewrite

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,20 +1,24 @@
 import { Button, Stack, Title } from "@mantine/core";
 import { PartyPopper } from "lucide-react";
 import Link from "next/link";
+import { CenteredSection } from "../components/CenteredSection";
 
 export default function NotFoundPage() {
     return (
-        <Stack align="center" pt={220} pb={320}>
-            <PartyPopper size={128}></PartyPopper>
+        <CenteredSection>
+            <Stack align="center" pt={220} pb={320}>
+                <PartyPopper size={128}></PartyPopper>
 
-            <Title>Error 404</Title>
-            <div>
-                No se ha podido encontrar este sitio web
-            </div>
+                <Title>Error 404</Title>
+                <div>
+                    No se ha podido encontrar esta página.
+                </div>
 
-            <Link href={"/"}>
-                <Button>Volver al inicio</Button>
-            </Link>
-        </Stack>
+                <Link href={"/"}>
+                    <Button>Volver al inicio</Button>
+                </Link>
+            </Stack>
+        </CenteredSection>
+
     );
 }
