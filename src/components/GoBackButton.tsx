78c4b1d7ff0cb3e5b729cb054063ef6e195a045c
Bastiasa <leysantqm@gmail.com>
2026-08-20 04:24:00 -0500

    Go Back Button disable mode

--- src/components/GoBackButton.tsx
+++ src/components/GoBackButton.tsx
@@ -2,6 +2,7 @@
 
 import { Button } from "@mantine/core";
 import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
 
 export function GoBackButton({
     children,
@@ -9,9 +10,14 @@ export function GoBackButton({
     children?: any;
 }) {
     const router = useRouter();
+    const [canGoBack, setCanGoBack] = useState(false);
 
+    useEffect(() => {
+        setCanGoBack(window.history.length > 1);
+    }, []);
     return (
         <Button
+            disabled={!canGoBack}
             onClick={() => router.back()}
             children={children}
         />
