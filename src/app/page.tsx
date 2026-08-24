96ad916bcda89c23133afc18e97bb18bc811b85c
Bastiasa <leysantqm@gmail.com>
2026-08-24 09:41:53 -0500

    Title change

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,13 +2,14 @@ import { Metadata } from "next";
 import { BuildYourPlanSection } from "../components/buildYourPlan/BuildYourPlanSection";
 import { Hero } from "../components/homepage/Hero";
 import { ServicesSection } from "../components/homepage/ServicesSection";
+import { VideoSection } from "../components/homepage/VideoSection";
 import {
     ENTERPRISE_DEFINED_SERVICES,
     ENTERPRISE_DEFINED_SERVICES_CATEGORIES,
 } from "../data/services";
-import { VideoSection } from "../components/homepage/VideoSection";
 
 export const metadata: Metadata = {
+    title: "Zona Mágica | Recreacionistas en Medellí, fiestas infantiles y más",
     alternates: {
         canonical: "https://zonamagica.site/",
     },
